40cc978417872cf87a2a27c9955beac144dbd648
ELIAS FONSECA MARTINS <eliasmartinsdev8@gmail.com>
2022-07-01 13:37:49 -0300

    sofrendo com js

---
+++ fundamentos/erro.js
@@ -0,0 +1,13 @@
+function tratarErroELancar(erro){
+  // throw new Error('...')
+  throw 10
+}
+function imprimirNomeGritado(obj){
+  try{
+    console.log(obj.name.toUpperCase()+'!!!')
+  }catch (e) {
+    tratarErroELancar(e)
+  }
+}
+const obj= {nome:'Roberto'}
+imprimirNomeGritado(obj)
