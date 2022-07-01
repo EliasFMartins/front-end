7433f7738701639834d01919b59da2e62d426fc8
ELIAS FONSECA MARTINS <eliasmartinsdev8@gmail.com>
2022-07-01 17:40:40 -0300

    k

---
+++ controle/for.js
@@ -0,0 +1,17 @@
+// let contador=1
+// let num=5
+// while(contador<=10){
+//   console.log(`${contador} vezes ${num} = ${contador*num}`)
+//   contador++
+// }
+// console.log('Fim')
+
+for( let i=1 ; i<=10 ; i++){
+  console.log(`i =${i}.`)
+}
+
+const notas=[6.5,7.4,9.8,8.1,7.7]
+for( i=0;i<notas.length; i++){
+  console.log(`Notas=${notas[i]}`)
+  //length conta o tamanho do array  o "i " vai se repetir ate q ele seja maior q o array mostrando todos os  numeros do array
+}
