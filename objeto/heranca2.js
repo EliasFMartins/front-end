c40cd550f091eb28a6e8af6baa747efb3a9c58e9
ELIAS FONSECA MARTINS <eliasmartinsdev8@gmail.com>
2022-07-04 11:55:19 -0300

    att

--- objeto/heranca2.js
+++ objeto/heranca2.js
@@ -5,3 +5,41 @@ const pai={__proto__:avo,Attr2:'B'}
 const filho={__proto__:pai,Attr3:'C'}
 
 console.log(filho.attr0,filho.Attr1,filho.Attr2,filho.Attr3)
+
+const carro={
+  velAtual:0,
+  velMax:200,
+  acelerarMais(delta){
+    if (this.velAtual + delta<=this.velMax){
+      this.velAtual+=delta
+    }else{
+      this.velAtual=this.velMax
+    }
+  },
+  status(){
+    return`${this.velAtual}Km/h de ${this.velMax}Km/h`
+  }
+}
+
+const ferrari={
+  modelo: 'F40',
+  velMax: 324 // Shadowing
+
+}
+
+const volvo={
+  modelo:'V40',
+status(){
+   return `${this.modelo}: ${super.status()}` 
+}
+}
+
+Object.setPrototypeOf(ferrari,carro)
+Object.setPrototypeOf(volvo,carro)
+
+console.log(ferrari)
+console.log(volvo)
+volvo.acelerarMais(100)
+console.log(volvo.status())
+ferrari.acelerarMais(300)
+console.log(ferrari.status())
