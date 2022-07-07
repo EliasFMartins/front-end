b8e87a66fc51e109ff30a4c2a2a65d7ebe8eac8b
ELIAS FONSECA MARTINS <eliasmartinsdev8@gmail.com>
2022-07-07 13:53:23 -0300

    att

---
+++ esnext/revisão01.js
@@ -0,0 +1,17 @@
+//let e const 
+{
+  var a=2
+  let b=3
+  console.log(b)
+}
+
+console.log(a)
+//destructuring
+const [l,e,...tras]="cod3r"
+console.log(l,e,tras)
+
+const [x,y]=[1,2,]
+console.log(x,y)
+
+const {idade:i,nome}={nome:'Ana',idade:8}
+console.log(i,nome)
